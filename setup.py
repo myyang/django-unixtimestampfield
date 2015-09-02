c8b1cadf42b207106ff95388f07d535b907ed2af
myyang <ymy1019@gmail.com>
2015-09-02 18:24:34 +0800

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,53 @@
+from distutils.core import setup
+from distutils.core import Command
+
+"""
+Copied and stole from https://github.com/bradjasper/django-jsonfield/blob/master/setup.py
+"""
+
+class TestCommand(Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        from django.conf import settings
+        settings.configure(
+            DATABASES={
+                'default': {'NAME': ':memory:', 'ENGINE': 'django.db.backends.sqlite3'}},
+            INSTALLED_APPS=('unixtimestampfield',)
+        )
+        from django.core.management import call_command
+        import django
+
+        if django.VERSION[:2] >= (1, 7):
+            django.setup()
+        call_command('test', 'unixtimestampfield')
+
+setup(name='django_unixtimestampfield',
+      version='0.1',
+      packages=['unixtimestampfield'],
+      license='MIT',
+      include_package_data=True,
+      author='Garfield.Yang',
+      author_email='ymy1019@gmail.com',
+      url='https://github.com/myyang/django-unixtimestampfield',
+      description='Unix timestamp (POSIX type) field',
+      long_description=open("README.md").read(),
+      install_requires=['Django >= 1.6'],
+      tests_require=['Django >= 1.6'],
+      cmdclass={'test': TestCommand},
+      classifiers=[
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.4',
+          'Framework :: Django',
+      ],
+      )
