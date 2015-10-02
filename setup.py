5eb143602b36bf15a796469ace28d8d99b5a1ac7
myyang <ymy1019@gmail.com>
2015-10-02 15:27:31 +0800

    update pypi

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ class TestCommand(Command):
         call_command('test', 'unixtimestampfield')
 
 setup(name='django-unixtimestampfield',
-      version='0.3',
+      version='0.3.3',
       packages=['unixtimestampfield'],
       license='MIT',
       author='Garfield.Yang',
